7c0643476e183abee86f11b582d4b7441fc29c61
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-04-07 14:15:38 +0200

    Add files via upload

---
+++ Zadatak.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SystemIO
+{
+    internal class klasaDirectory
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite putanju direktorija: ");
+            string putanja = Console.ReadLine();
+
+            //provjera da li direktorij postoji
+            if (Directory.Exists(putanja))
+            {
+                //ako direktorij postoji, ispisujemo sve datoteke u njemu
+                Console.WriteLine("Datoteke: ");
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+
+            //putanja za poddirektorij Test
+            string putanjaPoddirektorija = "\\Test";
+            // spajanje putanja u jednu
+            string novaPutanja = putanja + putanjaPoddirektorija;
+
+            //ako direktorij ne postoji, kreirat ćemo ga
+            if (!Directory.Exists(novaPutanja))
+            {
+                Directory.CreateDirectory(novaPutanja);
+            }
+            //ako direktorij postoji, ispisujemo poruku
+            else
+            {
+                Console.WriteLine("Direktorij već postoji!");
+            }
+
+            //ispis svih poddirektorija
+            Console.WriteLine("Poddirektoriji: ");
+            foreach (string poddirektorij
+                in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(poddirektorij);
+            }
+
+            Directory.Delete(novaPutanja);
+
+            Console.WriteLine("Nakon brisanja: ");
+            foreach (string poddirektorij
+                in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(poddirektorij);
+            }
+            Console.ReadKey();
+        }
+    }
+}
